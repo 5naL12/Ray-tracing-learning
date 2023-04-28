a0415d70090b2dcd3772be31dfd676f6fbef4f2b
5naL12 <948482379@qq.com>
2023-04-28 20:50:53 +0800

    Ray tracing in one weekend done

--- Learning/image/image/vec3.h
+++ Learning/image/image/vec3.h
@@ -148,6 +148,16 @@ inline vec3 random_in_hemisphere(const vec3& normal)
         return -in_unit_sphere;
 }
 
+inline vec3 random_in_disk()
+{
+    while (true)
+    {
+        auto p = vec3(random_double(-1.0, 1.0), random_double(-1, 1), 0);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
 inline vec3 reflect(const vec3& v, const vec3& n)
 {
     return v - 2 * dot(v, n) * n;
